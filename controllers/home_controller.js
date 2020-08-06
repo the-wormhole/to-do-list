5cee83604a973b35a335bda87f20e9a8196def03
Nayan <nayan.agg13@gmail.com>
2020-08-06 14:31:36 +0530

    Set up the home controller

---
+++ controllers/home_controller.js
@@ -0,0 +1,5 @@
+module.exports.home = function(req,res){
+
+    return res.send('<h1>List is coming in shape</h1>');
+    
+}
--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+//using express router
+app.use('/',require('./routes/index'));
+
+
+
+
 app.listen(port,function(err){
     if(err){
         console.log(`Error in running the server : ${err}`);
